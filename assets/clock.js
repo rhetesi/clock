7fa2ff2a33ca640d6a257f21ccd2a2d7786bd87b
rhetesi <rhetesi@gmail.com>
2020-12-01 11:03:43 +0100

    Feat: full HTML, mostly the CSS and some JS

--- assets/clock.js
+++ assets/clock.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function time() {
+    const date = new Date();
+    const display = document.querySelector('.clock__display');
+    
+    return date.toLocaleTimeString('hu-HU', { hour12: false })
+}
+
+console.log(time());
+
+//const timeOptions =
