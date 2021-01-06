ddff862cc07d821051af5a7ca00031f57a5ff560
rhetesi <rhetesi@gmail.com>
2021-01-06 17:43:13 +0100

    ESLinted :)

--- assets/clock.js
+++ assets/clock.js
@@ -1,11 +1,11 @@
-'use strict';
-
-function time() {
-    const date = new Date();
-    const display = document.querySelector('.clock__display');
-    const time = date.toLocaleTimeString('hu-HU', { hour12: false });
-    display.textContent = '';
-    display.insertAdjacentText('beforeend', time) 
+/* eslint-disable linebreak-style */
+function timer() {
+  const date = new Date();
+  const display = document.querySelector('.clock__display');
+  const time = date.toLocaleTimeString('hu-HU', { hour12: false });
+  display.textContent = '';
+  display.insertAdjacentText('beforeend', time);
 }
 
-let myTimer = setInterval(time, 1000);
+// eslint-disable-next-line no-unused-vars
+const myTimer = setInterval(timer, 1000);
